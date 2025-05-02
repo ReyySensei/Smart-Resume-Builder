e0b6e47ebe600c11643ed4462a1c1f269aad49a9
ReyyDev <reyy100101@gmail.com>
2025-05-02 09:21:59 +0800

    Initial commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -225,3 +225,4 @@ function previewImage(){
 function printCV(){
     window.print();
 }
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -19,3 +19,26 @@ $(document).ready(function(){
         isFirstItemUndeletable: true
     })
 })
+
+$(document).ready(function () {
+    $('#templateSelect').on('change', function () {
+        const selectedTemplate = $(this).val();
+        $('body').attr('data-template', selectedTemplate);
+        updateTemplateImages(selectedTemplate);
+        generateCV(); // refresh preview if needed
+    });
+
+    function updateTemplateImages(template) {
+        if (template === 'culinary') {
+            $('.navbar-brand-icon').attr('src', 'assets/images/chef-hat.png');
+        } else if (template === 'tech') {
+            $('.navbar-brand-icon').attr('src', 'assets/images/code-icon.png');
+        } else if (template === 'education') {
+            $('.navbar-brand-icon').attr('src', 'assets/images/book-icon.png');
+        } else if (template === 'design') {
+            $('.navbar-brand-icon').attr('src', 'assets/images/brush-icon.png');
+        } else {
+            $('.navbar-brand-icon').attr('src', 'assets/images/default-icon.png');
+        }
+    }
+});
